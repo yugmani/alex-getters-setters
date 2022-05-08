e8deadb15146d84845844af488a279fb53337f30
yugmani <yugmani@yahoo.com>
2022-05-07 23:51:34 -0700

    property getter

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // Import stylesheets
 import './style.css';
 
+//Getter method
+// *************************************
+
+//Create an object
+const dog = {
+  name: 'Jack',
+
+  // Create getter method
+  get getName() {
+    // returning dog's name
+    return `My dog's name is ${this.name}.`;
+  },
+};
+
+//Execute the getter method
+// Note: don't use parentheses to call getter method
+// console.log(dog.getName());  //Error: dog.getName is not a function
+
+console.log(dog.getName);
+//My dog's name is Jack.
